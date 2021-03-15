bf42df000a8bd62b1d35e284779cddb822213d84
Sameer Zopate <71779830+sameerzopate@users.noreply.github.com>
2021-03-15 11:14:43 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,27 @@
+//This is in App.js file
+import React from 'react';
+import './App.css';
+import Navbar from './components/navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import chart from './pages/Dashbord';
+import Reports from './pages/report';
+import Assets from './pages/Assets';
+
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={chart} />
+          <Route path='/reports' exact component={Reports} />
+          <Route path='/Assets' exact component={Assets} />
+        </Switch>
+      </Router>
+      
+    </>
+  );
+}
+
+export default App;
